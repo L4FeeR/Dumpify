a0e7d7b825a61b0237f5bf53378d93d2c36df58c
L4FeeR <muhamedlafeer837@gmail.com>
2024-10-22 13:13:47 +0530

    added required packages

---
+++ packages.sh
@@ -0,0 +1,15 @@
+packages=(
+	"tar",
+	"xz-utils",
+	"gzip",
+	"lolcat",
+	"zip",
+	"unzip"
+)
+ #tar - tar based compression
+ #xz  - xz based compression
+ #gzip - gunzip based compression
+ #lolcat - Text color
+ #zip - zipping files
+ #unzip - unzipping files
+	
