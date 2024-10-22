e173f93baa8eb123690a49d436375e70755b24f6
L4FeeR <muhamedlafeer837@gmail.com>
2024-10-22 13:00:17 +0530

    init main file : functionality menu

---
+++ Dumpify.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class Dumpify{
+	public static void main(String laf[]){
+	System.out.println("\n\tDumpify\n");
+	System.out.println("\t1.(E)ncode");
+	System.out.println("\t2.(D)ecode");
+	System.out.println("\t3.(C)heck (F)iles");
+	System.out.println("\t4.(P)roof (O)f (c)oncept");
+	System.out.println("\t0.(E)xit\n");
+
+	}
+}
